b41977a9c647b560508cad6d0984676cc2d6adda
AstureS <romart5@yandex.ru>
2016-07-03 23:32:57 +0300

    First cycle complete

---
+++ Task_4.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+def ln(a):
+	return math.log(a)
+
+def IsPositive(a):
+	if a > 0 : 
+		return True
+	else:
+		return False
+
+def ShowPositiveResult(a, b):
+	print("ln(%s) = %.6f" % (a, b))
+
+def ShowNegativeResult(a):
+	print("ln(%s) is illegal" % a)
+
+def CalculateLn(a):
+	arg = float(a)
+	if IsPositive(arg):
+		ShowPositiveResult(arg, ln(arg))
+	else:
+		ShowNegativeResult(arg)
+
+
+def FirstCycle():
+	for r in sys.argv[1:]:
+		CalculateLn(r)
+
+FirstCycle()
